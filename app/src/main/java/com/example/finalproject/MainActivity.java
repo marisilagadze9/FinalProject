c3ebfd3852d3bc797a982aea77d26b1265ef5647
marisilagadze9 <msilagadze63@gmail.com>
2026-01-08 00:58:27 +0400

    add home UI

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.finalproject;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +12,22 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    TextView title,date;
+    ImageButton add;
+    ScrollView scrol;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        title=findViewById(R.id.title);
+        add=findViewById(R.id.add);
+        date=findViewById(R.id.date);
+
+        scrol=findViewById(R.id.home);
+
+
     }
 }
